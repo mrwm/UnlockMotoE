0ae4b0095165c28775dbf964eafbb0a0a8956819
wiiliam <wiiliamchung360@gmail.com>
2016-06-05 16:09:49 -0700

    updated READE & renamed the script

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 An attempt to brute force a bootloader unlock on the verizon moto e XT1528 model
 
 # Prerequisites:
-`crunch`
+`crunch`<br>
+`bash`
+
+## TODO:
+- Remove using combinations with two or more consecutive characters that are the same.<br>
+Example: TABFSDFHHSDIFLJPHDYB #We do not want "HH" in our code
+- Make the script run faster
---
+++ force.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Count=1
+
+function0 () 
+{
+Command=$( crunch 20 20 ABCDEFGHIJKLMNOPQRSTUVWXYZ | grep -E "([A-Z])\1{1}" -v | head -n $Count | tail -n1 ) #Make the combination we need
+fastboot oem unlock $Command #Test to see if it works!
+
+#Debugging:
+#echo $Count
+#echo $Command
+Count=$(( $Count + 1 ))
+}
+
+while true; do
+function0
+done
